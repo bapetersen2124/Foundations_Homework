bd3c8aecdd8b2d378c1be883eaedde50b48f5250
bapetersen2124 <bapetersen2124@gmail.com>
2017-03-01 23:15:54 -0500

    JQuery Assignment 6 Work In Progress
    Edited “buy now button” and shopping cart underline. Working on
    mid-page nav section.

---
+++ Foundations_Homework/scripts.js
@@ -0,0 +1,39 @@
+// var listItems = $("li");
+// console.log(listItems);
+// // targeting all list items in HTML
+//
+// var myH1Selection = $(".span");
+// console.log(myH1Selection);
+//
+// $("ul").addClass("main-content");
+//
+// // targeting unordered lists, adding class "main content"
+//
+// var sectionSelection = $("section");
+// sectionSelection.text("this is new content!")
+// // text is a something you can call, will change text that exists in that target
+//
+// $("body").append("<div> This is a new div that wasn't here before! </div>")
+// // append will add to the page any content that you pass to it in parentheses; defaults to bottom of page
+
+$("#buy-now-button").hover(function() {
+  $("#buy-now-button").text("ADD TO CART!");
+}, function(){
+  $("#buy-now-button").text("BUY NOW!");
+});
+
+$("#buy-now-button").click(function() {
+  $("#buy-now-button").text("ADDED TO CART!");
+});
+
+$("#shopping-cart").hover(function() {
+  $("#shopping-cart").addClass("thick-underline");
+}, function (){
+  $("#shopping-cart").removeClass("thick-underline");
+});
+
+$(".mid-page-nav-bar").hover(function() {
+  $(this).addClass("thick-underline");
+}, function () {
+  $(this).removeClass("thick-underline");
+});
